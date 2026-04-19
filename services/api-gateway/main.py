3e6fcaf24b04e0ea01ab568b5cf97ee1db8114d7
Angel Osmar Perez Guevara <148883453+4ngel-0smar@users.noreply.github.com>
2026-04-19 22:12:00 +0000

    Add API Gateway

--- services/api-gateway/main.py
+++ services/api-gateway/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
+
+INGEST_URL = "http://ingest-service"
+PROCESS_URL = "http://processing-service"
 
 @app.get("/")
 def home():
     return {
-        "project": "Car Monitoring System",
-        "gateway": "running"
+        "service": "api-gateway",
+        "status": "running"
     }
+
+@app.get("/ingest")
+def ingest_root():
+    return requests.get(f"{INGEST_URL}/").json()
+
+@app.get("/process")
+def process_root():
+    return requests.get(f"{PROCESS_URL}/").json()
+
+@app.post("/analyze")
+def analyze(data: dict):
+    return requests.post(f"{PROCESS_URL}/analyze", json=data).json()
