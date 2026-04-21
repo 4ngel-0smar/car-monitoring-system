c2d27f3d559b941cea9c19d83935a1e32b3d8544
Angel Osmar Perez Guevara <148883453+4ngel-0smar@users.noreply.github.com>
2026-04-21 03:33:51 +0000

    Connect processing-service to PostgreSQL

--- services/processing-service/main.py
+++ services/processing-service/main.py
@@ -1,36 +1,65 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import psycopg2
+import os
 
 app = FastAPI()
 
+class Telemetry(BaseModel):
+    rpm: int
+    temp: float
+    battery: float
+
+def save_to_db(rpm, temp, battery, alerts):
+    conn = psycopg2.connect(
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"],
+        dbname=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASS"],
+        sslmode="require"
+    )
+
+    cur = conn.cursor()
+
+    cur.execute("""
+        INSERT INTO telemetry (rpm, temperature, battery, alerts)
+        VALUES (%s, %s, %s, %s)
+    """, (rpm, temp, battery, alerts))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
 @app.get("/")
-def home():
-    return {
-        "service": "processing-service",
-        "status": "running"
-    }
+def root():
+    return {"service":"processing-service","status":"running"}
 
 @app.post("/analyze")
-def analyze(data: dict):
-
-    rpm = data.get("rpm", 0)
-    temp = data.get("temp", 0)
-    battery = data.get("battery", 12)
+def analyze(data: Telemetry):
 
     alerts = []
 
-    if rpm > 5000:
+    if data.rpm > 6000:
         alerts.append("RPM alta")
 
-    if temp > 100:
+    if data.temp > 100:
         alerts.append("Temperatura alta")
 
-    if battery < 11.5:
+    if data.battery < 12:
         alerts.append("Bateria baja")
 
-    if len(alerts) == 0:
+    if not alerts:
         alerts.append("Sin alertas")
 
+    save_to_db(
+        data.rpm,
+        data.temp,
+        data.battery,
+        ", ".join(alerts)
+    )
+
     return {
-        "received": data,
+        "received": data.dict(),
         "alerts": alerts
     }
